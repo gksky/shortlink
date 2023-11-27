e83bbbf1e3e0016e005a52c30ffb7a75907322f6
Alexander Gutkovsky <a.gutkovsky@sms.by>
2023-11-27 13:32:31 +0300

    add: migration for links table

---
+++ database/migrations/2023_11_27_092410_create_links.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('uid')->nullable();
+            $table->string('title')->nullable();
+            $table->string('link', 2048);
+            $table->string('shortLink');
+            $table->unsignedInteger('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+};
