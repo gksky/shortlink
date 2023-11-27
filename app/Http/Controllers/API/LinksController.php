3c0885c5966fed499fdb8b3a01ab4ce745d7a170
Alexander Gutkovsky <a.gutkovsky@sms.by>
2023-11-27 14:07:39 +0300

    feat: create short link method

--- app/Http/Controllers/API/LinksController.php
+++ app/Http/Controllers/API/LinksController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Links;
+use Illuminate\Support\Facades\Auth;
 
 class LinksController extends Controller
 {
-    //
+    public function createShortLink(Request $request)
+    {
+        $link = new Links();
+        $link->uid = (Auth::user()) ? Auth::user()->id : NULL;
+        $link->title = $request->title;
+        $link->link = $request->link;
+        $link->shortLink = env("APP_URL") . ':' . env("SERVER_PORT") . '/' . uniqid();
+        $link->save();
+
+        return response()->json($link);
+    }
 }
